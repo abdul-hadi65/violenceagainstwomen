46e799536465879107010a7f63d8862ccef2c3c3
abdul-hadi65 <abdulhadismash@gmail.com>
2025-01-21 15:37:41 +0500

    Bar Plot

--- Code.py
+++ Code.py
@@ -9,3 +9,11 @@ print("Dataset Info:")
 data_cleaned.info()
 print("Summary Statistics:")
 print(data_cleaned.describe())
+
+# Visualization 1: Bar plot of average 'Value' by Gender
+plt.figure(figsize=(10, 6))
+sns.barplot(x='Gender', y='Value', data=data_cleaned, ci=None, palette='pastel')
+plt.title('Average Survey Value by Gender')
+plt.ylabel('Average Value')
+plt.xlabel('Gender')
+plt.show()
